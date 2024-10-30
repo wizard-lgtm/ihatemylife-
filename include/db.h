c706f404207177dfd50f5c7786b30bb521867fbe
wizard-lgtm <rippiten23@gmail.com>
2024-10-30 08:57:30 +0300

    connect cleanup fuctions added

--- include/db.h
+++ include/db.h
@@ -1,7 +1,11 @@
 #ifndef DB_H
 #define DB_H
+#include "types.h"
+
+void db_connect();
+void db_cleanup();
 void db_get_all_notes();
-void db_create_note();
+void db_create_note(Note *note);
 void db_get_note();
 void db_update_note();
 void db_delete_note();
